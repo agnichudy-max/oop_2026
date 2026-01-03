0beedf19d20e7ac77c2a2eca86ba2baf4da28c39
agnichudy-max <agni.chudy@gmail.com>
2026-01-03 10:08:33 +0000

    Internal version of the class Czlowiek

---
+++ klasy.py
@@ -0,0 +1,18 @@
+#Klasa
+#Istota
+#Zamysl
+
+class Czlowiek:
+    gatunek = "Homo Sapiens"
+    def __init__(self):
+        print("Niech powstanie Czlowiek")
+
+#powstanie obiektu / gotowanie z przepisu
+adam = Czlowiek() #a = 4 # a = int(4)
+print(adam.gatunek)
+
+#print(type(adam))
+#print(dir(Czlowiek))
+#print(dir(adam))
+
+
